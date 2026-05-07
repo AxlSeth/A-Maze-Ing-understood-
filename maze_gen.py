e9ea4f7831fed2340e1d8c360172752477d08384
Axl <axlramaroson25@gmail.com>
2026-05-07 16:48:37 +0300

    maze generation done

--- maze_gen.py
+++ maze_gen.py
@@ -4,37 +4,34 @@ WIDTH: int = 21
 HEIGHT: int = 12
 
 
-OPPOSITES: dict[str, str] = {"n":"s",
-                             "s":"n",
-                             "e":"w",
-                             "w":"e"}
+OPPOSITES: dict[str, str] = {"n": "s",
+                             "s": "n",
+                             "e": "w",
+                             "w": "e"}
 
 
-PATTERN: list[list[int]] = [[0,1,0,0,0,1,1,1],
-                            [0,1,0,0,0,0,0,1],
-                            [0,1,0,1,0,1,1,1],
-                            [0,1,1,1,0,1,0,0],
-                            [0,0,0,1,0,1,1,1]]
-#center of the 42 pattern is [y=2, x=4]
+PATTERN: list[list[int]] = [[0, 1, 0, 0, 0, 1, 1, 1],
+                            [0, 1, 0, 0, 0, 0, 0, 1],
+                            [0, 1, 0, 1, 0, 1, 1, 1],
+                            [0, 1, 1, 1, 0, 1, 0, 0],
+                            [0, 0, 0, 1, 0, 1, 1, 1]]
 
 
 MIN_WIDTH: int = len(PATTERN[0]) + 2
 MIN_HEIGHT: int = len(PATTERN) + 2
 
-#-------------------
 
 class Cell():
     def __init__(self) -> None:
-        self.walls = {"n" : 1,
-                      "s" : 1,
-                      "e" : 1,
-                      "w" : 1}
+        self.walls = {"n": 1,
+                      "s": 1,
+                      "e": 1,
+                      "w": 1}
         self.is_checked: int = 0
         self.is_pattern: int = 0
 
-#------------------
 
-def display_maze(maze: list[list[Cell]]):
+def display_maze(maze: list[list[Cell]]) -> None:
     height: int = len(maze)
     width: int = len(maze[0])
 
@@ -65,13 +62,11 @@ def display_maze(maze: list[list[Cell]]):
             print("   ", end="")
     print("+")
 
-#--------------------
 
 class MazeGenerator():
-    def __init__ (self) -> None:
+    def __init__(self) -> None:
         self.width = WIDTH
         self.height = HEIGHT
-
 
     def apply_pattern(self,
                       maze: list[list[Cell]]) -> None:
@@ -80,53 +75,46 @@ class MazeGenerator():
         for py in range(len(PATTERN)):
             for px in range(len(PATTERN[0])):
                 if PATTERN[py][px] == 1:
-                    maze[oy + py][ox + px].is_pattern = 1 
-
+                    maze[oy + py][ox + px].is_pattern = 1
 
     def get_neighbors(self,
                       maze: list[list[Cell]],
                       y: int,
-                      x: int) -> list[tuple[str ,int , int]]:
+                      x: int) -> list[tuple[str, int, int]]:
         valid_directions: list[tuple[str, int, int]] = []
         if ((y + 1) < HEIGHT and
             maze[y + 1][x].is_checked == 0 and
-            maze[y + 1][x].is_pattern == 0):
-            valid_directions.append(("s" ,y + 1 , x))
+                maze[y + 1][x].is_pattern == 0):
+            valid_directions.append(("s", y + 1, x))
         if ((y - 1) >= 0 and
             maze[y - 1][x].is_checked == 0 and
-            maze[y - 1][x].is_pattern == 0):
-            valid_directions.append(("n" ,y - 1 , x))
+                maze[y - 1][x].is_pattern == 0):
+            valid_directions.append(("n", y - 1, x))
         if ((x + 1) < WIDTH and
             maze[y][x + 1].is_checked == 0 and
-            maze[y][x + 1].is_pattern == 0):
+                maze[y][x + 1].is_pattern == 0):
             valid_directions.append(("e", y, x + 1))
         if ((x - 1) >= 0 and
             maze[y][x - 1].is_checked == 0 and
-            maze[y][x - 1].is_pattern == 0):
+                maze[y][x - 1].is_pattern == 0):
             valid_directions.append(("w", y, x - 1))
         return (valid_directions)
-
 
     def create_paths(self, maze: list[list[Cell]]) -> None:
         maze[0][0].is_checked = 1
         stack: list[tuple[int, int]] = [(0, 0)]
 
-        while (len(stack) != 0):
+        while stack:
             y, x = stack[-1]
-            valid_directions: list[tuple[str, int, int]] = self.get_neighbors(maze, y, x)
+            valid_directions = self.get_neighbors(maze, y, x)
             if len(valid_directions) == 0:
                 stack.pop()
             else:
-                direction: str
-                ny: int
-                nx: int
                 direction, ny, nx = random.choice(valid_directions)
-
-
-
-                
-
-                
+                maze[ny][nx].is_checked = 1
+                maze[y][x].walls[direction] = 0
+                maze[ny][nx].walls[OPPOSITES[direction]] = 0
+                stack.append((ny, nx))
 
     def generate(self,
                  width: int,
@@ -143,11 +131,12 @@ class MazeGenerator():
         self.create_paths(maze)
         display_maze(maze)
         return (maze)
-            
-#----------------------
+
+
 def main() -> None:
     maze_gen: MazeGenerator = MazeGenerator()
     maze_gen.generate(WIDTH, HEIGHT)
+
 
 if __name__ == "__main__":
     main()
